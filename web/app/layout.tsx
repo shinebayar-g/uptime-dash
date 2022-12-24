8f91aa1e9f35c063d0744b75a6d2e6b29d3e2bb5
Shinebayar Gansukh <3091558+shinebayar-g@users.noreply.github.com>
2022-12-23 17:56:25 -0800

    update dependencies, cleanup

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,18 +1,12 @@
-import './globals.css'
+import { Inter } from '@next/font/google'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+    return (
+        <html lang="en" className={inter.className}>
+            <head />
+            <body>{children}</body>
+        </html >
+    )
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,4 @@
-'use client';
-import { useState, useEffect } from 'react'
 import { createConnectTransport, createPromiseClient } from '@bufbuild/connect-web'
-
-import Image from 'next/image'
-import styles from './page.module.css'
 import { UptimeService } from '../gen/uptime_connectweb'
 
 const transport = createConnectTransport({
@@ -13,42 +8,7 @@ const transport = createConnectTransport({
 const client = createPromiseClient(UptimeService, transport)
 
 export default function Home() {
-    const [data, setData] = useState(null)
-    const [isLoading, setLoading] = useState(false)
-
-    useEffect(() => {
-        setLoading(true)
-        client.getAllTargets({})
-            .then((res) => res.toJSON())
-            .then((data: any) => {
-                setData(data)
-                setLoading(false)
-            })
-    }, [])
-
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
-
     return (
-        <div>
-            <main className={styles.main}>
-                <div>targets:
-                    {JSON.stringify(data)}
-                </div>
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <span className={styles.logo}>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
-                </a>
-            </footer>
-        </div>
+        <h1>Hello World</h1>
     )
 }
